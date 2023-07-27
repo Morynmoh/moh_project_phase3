22576e514e38264f3ea7b48835f7c43159dce84e
Maureen <morynmoh@gmail.com>
2023-07-27 20:38:04 +0300

    login styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,21 +4,18 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 const Login = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  
-
-const logUser = async () => {
-  const response = await axios.post('http://localhost:9292/login', {
-        user_name: userName,
-        password: password,
-      });
-      console.log(response.data);
-  }
+  const logUser = async () => {
+    const response = await axios.post('http://localhost:9292/login', {
+      user_name: userName,
+      password: password,
+    });
+    console.log(response.data);
+  };
 
   const handleLogin = () => {
     if (userName === '' || password === '') {
@@ -26,49 +23,49 @@ const logUser = async () => {
       return;
     }
 
-    logUser()
+    logUser();
     navigate('/Home', { state: { userName: userName } });
+  };
 
-      
-      
+  const buttonStyle = {
+    margin: '10px 0',
+    padding: '8px 16px',
+    border: '2px solid #0E2954',
+    backgroundColor: '#0E2954',
+    color: '#fff',
+    textDecoration: 'none',
+    borderRadius: '4px',
   };
 
   return (
-    <section>
-        <div className="register">
-            <div className="col-1">
-                <h2>Login</h2>
-                
-                <form id='form' className='flex flex-col' 
-                onSubmit={handleLogin}
-                >
-                    <input 
-                    type="text" 
-                    placeholder='username' 
-                    value={userName}
-                    onChange={(e) => setUserName(e.target.value)}
-                    required
-                    />
-                    <input 
-                    type="password" 
-                    placeholder='password' 
-                    value={password}
-                    required
-                    minLength={8}
-                    onChange={(e) => setPassword(e.target.value)}
-                    />
-                    
-                    <button className='btn'type="submit">Log In</button>
-                    <h6>If you have not registered</h6>
-                    <button className='btn' onClick={() => navigate('/')}>Go To Register</button>
-                </form>
-
-            </div>
-            <div className="col-2">
-                <img src={bgImg} alt="" />
-            </div>
-        </div>
+    <section style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff', border: '1px solid #ddd', borderRadius: '8px', padding: '20px', width: '400px' }}>
+        <h2 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>Login</h2>
+        <form id='form' className='flex flex-col' onSubmit={handleLogin} >
+          <input
+            type="text"
+            placeholder='username'
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+            style={{ margin: '10px 0', padding: '8px', width: '100%', boxSizing: 'border-box' }}
+          />
+          <input
+            type="password"
+            placeholder='password'
+            value={password}
+            required
+            minLength={8}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ margin: '10px 0', padding: '8px', width: '100%', boxSizing: 'border-box' }}
+          />
+          <button className='btn' type="submit" style={buttonStyle}>Log In</button>
+          <h6 style={{ marginTop: '10px' }}>If you have not registered</h6>
+          <button className='btn' onClick={() => navigate('/')} style={buttonStyle}>Go To Register</button>
+        </form>
+      </div>
     </section>
   )
 }
+
 export default Login;
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -5,70 +5,67 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
-
 const Signin = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-const createUser = async () => {
-  const response = await axios.post('http://localhost:9292/register', {
-        user_name: userName,
-        password: password,
-        
-      });
-      console.log(response.data);
-    }
+  const createUser = async () => {
+    const response = await axios.post('http://localhost:9292/register', {
+      user_name: userName,
+      password: password,
+    });
+    console.log(response.data);
+  };
 
   const handleRegister = () => {
     if (userName === '' || password === '') {
       alert('Please enter a username and password.');
       return;
     }
-  
-    createUser() 
-      navigate('/Login');
-    
+
+    createUser();
+    navigate('/Login');
+  };
+
+  const buttonStyle = {
+    margin: '10px 0',
+    padding: '8px 16px',
+    border: '2px solid #0E2954',
+    backgroundColor: '#0E2954', 
+    color: '#fff', 
+    textDecoration: 'none',
+    borderRadius: '4px',
   };
 
   return (
-    <section>
-        <div className="register">
-            <div className="col-1">
-                <h2>Register User</h2>
-                <span>register and enjoy the service</span>
-
-                <form id='form' className='flex flex-col'onSubmit={handleRegister}>
-                    <input 
-                    type="text" 
-                    placeholder='username' 
-                    value={userName}
-                    onChange={(e) => setUserName(e.target.value)}
-                    required
-                    />
-                    <input 
-                    type="password" 
-                    placeholder='password' 
-                    value={password}
-                    minLength={8}
-        
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    />
-                    
-                    <button className='btn' type="submit">Register</button>
-                    
-                    <h6>If you already have an account, you can Login</h6>
-                    <Link to='/Login' className='btn'>Go To Login</Link>
-                    
-                    </form>
-
-            </div>
-            <div className="col-2">
-                <img src={bgImg} alt="" />
-            </div>
-        </div>
+    <section style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff', border: '1px solid #ddd', borderRadius: '8px', padding: '20px' }}>
+        <h2 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>Register User</h2>
+        <span>register and enjoy the service</span>
+        <form id='form' className='flex flex-col' onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder='username'
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          required
+          style={{ margin: '10px 0', padding: '8px', width: '100%', boxSizing: 'border-box' }}
+        />
+        <input
+          type="password"
+          placeholder='password'
+          value={password}
+          minLength={8}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          style={{ margin: '10px 0', padding: '8px', width: '100%', boxSizing: 'border-box' }}
+        />
+        <button className='btn' type="submit" style={buttonStyle}>Register</button>
+        <h6 style={{ marginTop: '10px' }}>If you already have an account, you can Login</h6>
+        <Link to='/Login' className='btn' style={buttonStyle}>Go To Login</Link>
+      </form>
+      </div>
     </section>
   )
 }
